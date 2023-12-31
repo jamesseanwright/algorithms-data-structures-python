fb4f0bd74ff5d0ade63063066e51f2d1982ed598
James Wright <jsw@mahoganyteeth.com>
2023-12-31 16:11:13 +0000

    Simplifies is_balanced() impl

--- a_basic_data_structures/parens.py
+++ a_basic_data_structures/parens.py
@@ -1,21 +1,16 @@
 from a_basic_data_structures.stack import Stack
 
+
 def is_balanced(input: str) -> bool:
     s = Stack()
-    balanced = True
-    i = 0
 
-    while i < len(input) and balanced:
-        symbol = input[i]
-
+    for symbol in input:
         if symbol == "(":
             s.push(symbol)
         elif symbol == ")":
             if s.is_empty():
-                balanced = False
+                return False
             else:
                 s.pop()
 
-        i += 1
-
-    return balanced and s.is_empty()
+    return s.is_empty()
