b701d50ce7c86d5a140226b8b010d851050513f0
James Wright <jsw@mahoganyteeth.com>
2023-12-31 16:09:20 +0000

    Implements is_balanced

--- a_basic_data_structures/parens.py
+++ a_basic_data_structures/parens.py
@@ -1,2 +1,21 @@
+from a_basic_data_structures.stack import Stack
+
 def is_balanced(input: str) -> bool:
-    raise NotImplementedError()
+    s = Stack()
+    balanced = True
+    i = 0
+
+    while i < len(input) and balanced:
+        symbol = input[i]
+
+        if symbol == "(":
+            s.push(symbol)
+        elif symbol == ")":
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+        i += 1
+
+    return balanced and s.is_empty()
