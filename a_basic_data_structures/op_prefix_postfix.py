f68a55e69aa9d5f8a1442c2cf7d2d9bd59ea4147
James Wright <jsw@mahoganyteeth.com>
2024-01-08 20:44:03 +0000

    WIP to_postfix() implementation

--- a_basic_data_structures/op_prefix_postfix.py
+++ a_basic_data_structures/op_prefix_postfix.py
@@ -1,2 +1,36 @@
+from a_basic_data_structures.stack import Stack
+
+operators = "-+*/"
+
+
 def to_postfix(expr: str) -> str:
-    raise NotImplementedError()
+    ops = Stack()
+    out = ""
+
+    for char in expr:
+        if char == "(":
+            ops.push(char)
+
+        elif char == ")":
+            while ops.peek() != "(":
+                out += ops.pop()
+
+            out += ops.pop()  # TODO: dedupe statement
+
+        elif char in operators:
+            while (
+                not ops.is_empty()
+                and ops.peek() in operators
+                and operators.index(ops.peek()) >= operators.index(char)
+            ):
+                out += ops.pop()
+
+            ops.push(char)
+
+        else:
+            out += char
+
+    while not ops.is_empty():
+        out += ops.pop()
+
+    return out
