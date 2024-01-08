8abd9d80e514a59cc6e4fcc32c897b3be5f82b87
James Wright <jsw@mahoganyteeth.com>
2024-01-08 20:07:27 +0000

    Adds test and stub for expression postfix converter

---
+++ a_basic_data_structures/test_prefix_postfix.py
@@ -0,0 +1,9 @@
+import unittest
+from a_basic_data_structures.op_prefix_postfix import to_postfix
+
+
+class TestToPostfix(unittest.TestCase):
+    def test_to_postfix_converts_infix_expression_to_postfix(self):
+        self.assertEqual(to_postfix("(A + B) * C"), "A B + C *")
+        self.assertEqual(to_postfix("A + B * C + D"), " A B C * + D +")
+        self.assertEqual(to_postfix("(A + B) * (C + D)"), " A B + C D + *")
