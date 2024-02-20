4b73af5e90ea402d3de3fee7c171052f9d98cf60
James Wright <hi@james.engineering>
2024-02-20 17:30:19 +0000

    Implements happy-path postfix expression evaluation

--- a_basic_data_structures/eval_postfix_expr.py
+++ a_basic_data_structures/eval_postfix_expr.py
@@ -1,5 +1,29 @@
 from a_basic_data_structures.stack import Stack
 
+operators = "-+*/"
+
 
 def eval_postfix_expr(expr: str) -> int:
-    raise NotImplementedError()
+    operands = Stack()
+    tokens = expr.split(" ")
+
+    for token in tokens:
+        if token in operators:
+            res = evaluate(token, operands.pop(), operands.pop())
+            operands.push(res)
+        else:
+            operands.push(int(token))
+
+    return operands.pop()
+
+
+def evaluate(operator: str, right: int, left: int) -> int:
+    match operator:
+        case "/":
+            return left / right
+        case "*":
+            return left * right
+        case "+":
+            return left + right
+        case "-":
+            return left - right
